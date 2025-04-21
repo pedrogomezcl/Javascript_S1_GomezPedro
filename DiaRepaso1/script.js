78ec08b33edd4a07cbdc685e4f196ba986bdaa6c
Pedro Felipe Gomez Bonilla <pedro@gomez.com>
2025-04-21 09:14:11 -0500

    fix: :fire: CORS error

--- DiaRepaso1/script.js
+++ DiaRepaso1/script.js
@@ -8,22 +8,35 @@ console.log("Holii");
 
 console.log(json["file"]); */
 
-function fetchCoffee(){
+function fetchPoke(){
     let xhr = new XMLHttpRequest();
-
-    let link= `https://coffee.alexflipnote.dev/random.json`;
+    let numPokemon=29;
+    let link= `https://pokeapi.co/api/v2/pokemon/${numPokemon}`;
     //Primer paso es crear(abrir) la solicitud
     xhr.open('GET',link,true);
-    xhr.setRequestHeader("Access-Control-Allow-Origin", "*");
     //Segundo paso es decir qué hacer con esa solicitud
     xhr.onreadystatechange = function(){
         if(this.status ==200){
             let respuesta = JSON.parse(this.responseText);
-            console.log(respuesta);
+            console.log(respuesta["sprites"]["front_default"]);
+            displayPoke(respuesta);
         }
     };
     //El tercer paso es ejecutar la solicitud
     xhr.send();
 }
 
-fetchCoffee();
+fetchPoke();
+
+function displayPoke(data){
+    let informacionHTML = document.getElementById('daticos');
+    if(data.response == "error"){
+        informacionHTML.innerHTML=`<p>Esto no funcionó :sadfeis:</p>`;
+    }else{
+        
+        informacionHTML.innerHTML=`
+        <p>${data["name"]}</p>
+        <img src=${data["sprites"]["front_default"]} />
+        `
+    }
+}
